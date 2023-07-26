0599dc6b4f8a94acd739fa0e5f1ba627c6d6d94b
Joe-Stephens74 <joe.stephens@roweit.co.uk>
2023-07-26 09:31:00 +0100

    Update package name

--- ckan-cli/tdh_package.py
+++ ckan-cli/tdh_package.py
@@ -1,9 +1,9 @@
 # Package info
-PACKAGE_NAME = 'CKAN export - json'
+PACKAGE_NAME = 'WECA TDH CKAN import/export package'
 VERSION_NO = 1.0
 
 # Import/export filenames
-INFO_FILENAME = "info.json"
-DATASETS_FILENAME = "dataset.json"
+INFO_FILENAME = 'info.json'
+DATASETS_FILENAME = 'dataset.json'
 PUBLISHERS_FILENAME = 'publishers.json'
 TOPICS_FILENAME = 'topics.json'
