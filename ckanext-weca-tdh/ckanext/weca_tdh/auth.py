4d9289a01f3e78aa1becda8c1421795f7f31fb5a
Joe-Stephens74 <joe.stephens@roweit.co.uk>
2023-11-23 18:03:16 +0000

    Redirect user to destination

--- ckanext-weca-tdh/ckanext/weca_tdh/auth.py
+++ ckanext-weca-tdh/ckanext/weca_tdh/auth.py
@@ -20,7 +20,7 @@ class ADAuth():
                 user = User.get_or_create_ad_user(claims_map)
 
             ADAuth._login_to_ckan(user)
-            return toolkit.redirect_to('dashboard.datasets')
+            return toolkit.redirect_to(request.args.get('referrer', default='dashboard.datasets'))
 
         except Exception as e:
             flash(f"Authorisation failed: {e} {C.ALERT_MESSAGE_SUPPORT}.", category='alert-danger')
