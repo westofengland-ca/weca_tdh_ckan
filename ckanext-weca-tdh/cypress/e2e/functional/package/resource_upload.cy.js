99d0b6239562cd82fa3a0de05f19fd8b77b0a8c0
Joe-Stephens74 <joe.stephens@roweit.co.uk>
2024-05-03 16:27:26 +0100

    Add e2e test for upload form

---
+++ ckanext-weca-tdh/cypress/e2e/functional/package/resource_upload.cy.js
@@ -0,0 +1,85 @@
+describe('Upload file page', () => {
+
+  before(() => {
+    cy.deleteAllData()
+    cy.createTestData()
+    
+  })
+
+  after(() => {
+    cy.deleteTestData()
+    cy.logoutUser('user/login')
+  })
+
+  beforeEach(() => {
+    cy.fixture('datasets.json').then((datasets) => {
+      // login
+      cy.loginUser(`dataset/${datasets[0].name}/upload_form/${datasets[0].resources[0].id}`)
+    })
+  })
+
+  it('should display all form fields', () => {
+    cy.get('#field-resource-upload').should('exist');
+    cy.get('#field-author').should('exist');
+    cy.get('#field-author-email').should('exist');
+    cy.get('#field-description').should('exist');
+    cy.get('#submit-button').should('exist');
+  });
+
+  it('should not submit form if required fields are empty', () => {
+    let submitted = false
+    cy.get('#form-upload').invoke('submit', (e) => {
+      // do not actually submit the form
+      e.preventDefault()
+      submitted = true
+    })
+
+    cy.get('#form-upload').within(() => {
+      cy.get('#field-resource-upload').then($el => $el[0].checkValidity()).should('be.false')
+      cy.get('#field-resource-upload').invoke('prop', 'validationMessage')
+        .should('equal', 'Please select a file.')
+
+      cy.get('#field-author').then($el => $el[0].checkValidity()).should('be.false')
+      cy.get('#field-author').invoke('prop', 'validationMessage')
+        .should('equal', 'Please fill out this field.')
+
+      cy.get('#field-author-email').then($el => $el[0].checkValidity()).should('be.false')
+      cy.get('#field-author-email').invoke('prop', 'validationMessage')
+        .should('equal', 'Please fill out this field.')
+
+      cy.get('#submit-button').click()
+    }).then(() => {
+      expect(submitted, 'form submitted').to.be.false
+    })
+  });
+
+  it('should submit form when all required fields are filled', () => {
+    let submitted = false
+    cy.get('#form-upload').invoke('submit', (e) => {
+      // do not actually submit the form
+      e.preventDefault()
+      submitted = true
+    })
+
+    cy.get('#form-upload').within(() => {
+      cy.get('#field-resource-upload').selectFile('cypress/fixtures/datasets.json') // Attach a file
+        .then($el => $el[0].checkValidity()).should('be.true');
+      cy.get('#field-resource-upload').invoke('prop', 'validationMessage')
+        .should('equal', '')    
+        
+      cy.get('#field-author').type('John Doe')
+        .then($el => $el[0].checkValidity()).should('be.true'); // Fill author field
+      cy.get('#field-author').invoke('prop', 'validationMessage')
+        .should('equal', '')    
+
+      cy.get('#field-author-email').type('john@example.com')
+        .then($el => $el[0].checkValidity()).should('be.true'); // Fill author email field
+      cy.get('#field-author-email').invoke('prop', 'validationMessage')
+        .should('equal', '')
+
+      cy.get('#submit-button').click()
+    }).then(() => {
+      expect(submitted, 'form submitted').to.be.true
+    })
+  })
+})
--- ckanext-weca-tdh/cypress/support/commands.js
+++ ckanext-weca-tdh/cypress/support/commands.js
@@ -242,7 +242,7 @@ Cypress.Commands.add('getTopicDatasets', (topic) => {
     })
 })
 
-Cypress.Commands.add('getPackage', (dataset) => {
+Cypress.Commands.add('getPackage', (datasetName) => {
   const apiKey = Cypress.env('API_KEY')
   const url = Cypress.config().baseUrl
 
@@ -255,7 +255,7 @@ Cypress.Commands.add('getPackage', (dataset) => {
       'User-Agent': 'CKAN-CLI'
     },
     body: {
-      id: "bus_timetable"
+      id: datasetName
     }
     
   }).then(response => {
@@ -270,5 +270,12 @@ Cypress.Commands.add('loginUser', (callback) => {
   cy.get('input[id="field-login"]').type(Cypress.env('CKAN_USERNAME'))
   cy.get('input[id="field-password"]').type(Cypress.env('CKAN_PASSWORD'))
   cy.get('[data-cy="login-button"]').click()
+  cy.visit(`${url}/${callback}`)
+})
+
+Cypress.Commands.add('logoutUser', (callback) => {
+  const url = Cypress.config().baseUrl
+
+  cy.visit(`${url}/user/_logout`)
   cy.visit(`${url}/${callback}`)
 })
