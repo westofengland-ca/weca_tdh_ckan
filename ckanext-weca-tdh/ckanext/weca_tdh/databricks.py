4deed8e92cd997bc6e936cb0c0ed57db5b7eec46
Joe Stephens <joe.stephens@roweit.co.uk>
2025-01-29 10:45:01 +0000

    Add databricks auth and download

---
+++ ckanext-weca-tdh/ckanext/weca_tdh/databricks.py
@@ -0,0 +1,217 @@
+import base64
+import hashlib
+import json
+import logging
+import os
+import threading
+import time
+import uuid
+
+import ckan.plugins.toolkit as toolkit
+import requests
+from flask import (
+    Blueprint,
+    after_this_request,
+    flash,
+    jsonify,
+    request,
+    send_file,
+    session,
+)
+
+import ckanext.weca_tdh.config as C
+from ckanext.weca_tdh.upload import BlobStorage
+
+log = logging.getLogger(__name__)
+databricksbp = Blueprint('databricks', __name__)
+task_statuses = {}
+download_directory = "/tmp"
+
+
+def oauth_code_verify_and_challenge() -> tuple[str, str]:
+    uuid1 = uuid.uuid4()
+    uuid_str1 = str(uuid1).upper()
+    
+    code_verifier = uuid_str1 + "-" + uuid_str1
+    
+    code_challenge = base64.urlsafe_b64encode(
+        hashlib.sha256(code_verifier.encode()).digest()).decode('utf-8')
+    code_challenge = code_challenge.replace('=', '')
+    
+    return code_verifier, code_challenge
+
+
+def download_file_from_url(task_id, file_url, output_path, access_token):
+    try:
+        task_statuses[task_id] = {"status": "in_progress", "message": "Download started."}
+        headers = {
+            'Authorization': f'Bearer {access_token}'
+        }
+
+        with requests.get(file_url, headers=headers, stream=True) as response:
+            response.raise_for_status()
+            with open(output_path, 'wb') as file:
+                for chunk in response.iter_content(chunk_size=8192):
+                    if chunk:
+                        file.write(chunk)
+
+        task_statuses[task_id] = {"status": "completed", "message": "Download completed.", "file_path": output_path}
+    except requests.exceptions.HTTPError as e:
+        message = "Unauthorised access. Contact the Data Owner." if e.response.status_code == 403 else str(e)
+        task_statuses[task_id] = {"status": "error", "message": f"Download failed: {message}"}
+    except requests.exceptions.ConnectionError:
+        task_statuses[task_id] = {"status": "error", "message": "Invalid URL. Contact the Data Owner."}
+    except Exception as e:
+        task_statuses[task_id] = {"status": "error", "message": f"Download failed: {e}"}
+    
+
+@databricksbp.route('/databricks/download/status', methods=['POST'])
+def get_task_status():
+    data = request.get_json()
+    task_id = data.get("task_id")
+    
+    if not task_id:
+        flash("Failed to get download status: Task ID is required.", category='alert-danger')
+        return jsonify({"status": "error", "message": "task_id is required."}), 400
+
+    if task_id in task_statuses:
+        task_info = task_statuses[task_id]
+        if task_info["status"] == "error":
+            flash(task_info["message"], category='alert-danger') 
+        return jsonify(task_statuses[task_id])
+    else:
+        flash("Failed to get download status: Task ID not found.", category='alert-danger')
+        return jsonify({"status": "not_found", "message": "Task ID not found."}), 404
+
+
+@databricksbp.route('/databricks/download/<task_id>', methods=['GET'])
+def download_file(task_id):
+    if task_id not in task_statuses:
+        flash("Failed to download file: Task ID not found.", category='alert-danger')
+        return jsonify({"status": "error", "message": "Task ID not found."}), 404
+
+    task_info = task_statuses[task_id]
+    if task_info["status"] != "completed":
+        return jsonify({"status": task_info["status"], "message": task_info.get("message", "Task not ready.")})
+
+    file_path = task_info["file_path"]
+    if not os.path.exists(file_path):
+        flash("Failed to download file: file not found.", category='alert-danger')
+        return jsonify({"status": "error", "message": "File not found on server."}), 404
+
+    filename = os.path.basename(file_path)
+    
+    # Remove temporary file
+    @after_this_request
+    def cleanup(response):
+        try:
+            os.remove(file_path)
+            del task_statuses[task_id]
+        except Exception as e:
+            log.error(f"Error deleting file {file_path}: {e}")
+        return response
+    
+    return send_file(file_path, as_attachment=True, download_name=filename)
+
+
+class DatabricksWorkspace(object):
+    def __init__(self):
+        self.host = C.TDH_CONNECT_ADDRESS_HOST
+        self.warehouse_id = str(C.TDH_CONNECT_ADDRESS_PATH).split('/')[-1]
+        
+    def authorise(self):
+        try:
+            # Get workspace authorisation code for user
+            auth_code = self.get_workspace_auth_code()
+            
+            # Get auth code verifier
+            code_verifier = session.get('code_verifier')
+            
+            # Get/set workspace access token for user
+            access_token = self.generate_workspace_access_token(code_verifier, auth_code)
+            session['access_token'] = access_token
+            
+            # Redirect back to resource page
+            referrer = session.get('referrer')
+            return toolkit.redirect_to(referrer)
+
+        except Exception as e:
+            flash(f"Databricks authorisation failed: {e}", category='alert-danger')
+
+    @staticmethod
+    def get_workspace_auth_code() -> str:  
+        return request.args.get('code')
+    
+    @staticmethod
+    def get_workspace_access_token() -> str:
+        return session.get('access_token')
+
+    def generate_workspace_access_token(self, code_verifier: str, auth_code: str) -> str:
+        base_url = f"https://{self.host}/oidc/v1/token"
+        client_id = C.DB_APP_CLIENT_ID
+        redirect_url = C.DB_APP_REDIRECT_URL
+
+        data = {
+            "client_id": client_id,
+            "grant_type": "authorization_code",
+            "scope": "all-apis offline_access",
+            "redirect_uri": redirect_url,
+            "code_verifier": code_verifier,
+            "code": auth_code
+        }
+
+        try:
+            response = requests.post(base_url, data=data)
+            response_dict = response.json()
+            return response_dict.get("access_token")
+        except Exception as e:
+            raise Exception(f'failed to get Workspace access token. {e}')
+        
+    def start_download(self):
+        try:
+            data = request.get_json()
+            resource_id = data.get("resource_id")
+            catalog = self.get_catalog_files_local(resource_id)
+            file_name = catalog.get('file_name')
+            
+            file_url = f"https://{self.host}/api/2.0/fs/files/Volumes/ \
+                        {catalog.get('catalog_name')}/{catalog.get('schema_name')}/ \
+                        {catalog.get('volume_name')}/{file_name}"
+            
+            output_path = os.path.join(download_directory, file_name)
+            access_token = self.get_workspace_access_token()
+
+            if not file_url:
+                return jsonify({"status": "error", "message": "file_url is required."}), 400
+
+            task_id = str(int(time.time()))  # Generate unique task ID
+            thread = threading.Thread(target=download_file_from_url, args=(task_id, file_url, output_path, access_token))
+            thread.daemon = True
+            thread.start()
+
+            download_url = f"/databricks/download/{task_id}"
+            return jsonify({"task_id": task_id, "download_url": download_url, "message": "Download started."})
+        except Exception as e:
+            flash(f"Download failed: {e}.", category='alert-danger')
+            
+    @staticmethod
+    def get_catalog_files(resource_id):
+        json_data = BlobStorage().download_blob_as_json('databricks.json')
+        catalog_files = json_data.get("catalog_files", {})
+
+        return catalog_files.get(resource_id, None)
+    
+    @staticmethod
+    def get_catalog_files_local(resource_id):
+        file_path = "/srv/app/src_extensions/ckanext-weca-tdh/ckanext/weca_tdh/config/databricks.json"
+        with open(file_path, 'r') as file:
+            data = json.load(file) 
+
+        catalog_files = data.get("catalog_files", {})
+        return catalog_files.get(resource_id, None)
+
+
+databricksbp.add_url_rule('/databricks/auth', 
+                          view_func=DatabricksWorkspace().authorise)
+databricksbp.add_url_rule('/databricks/download/start', 
+                          view_func=DatabricksWorkspace().start_download, methods=['POST'])
