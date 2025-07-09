7f6ca9da80193fce3ae407f0699a36487351319a
Joe Stephens <joe.stephens@roweit.co.uk>
2025-07-09 14:31:59 +0100

    Update landing page

--- ckanext-weca-tdh/ckanext/weca_tdh/lib/helpers.py
+++ ckanext-weca-tdh/ckanext/weca_tdh/lib/helpers.py
@@ -130,7 +130,7 @@ def get_data_quality_markings() -> list:
 
 def get_featured_datasets(limit_new: int, limit_upcoming: int) -> dict:
     data_dict = {
-        'fq': '-availability:upcoming',
+        'fq': 'availability:available',
         'sort': 'metadata_created desc',
         'rows': limit_new
     }
@@ -139,6 +139,7 @@ def get_featured_datasets(limit_new: int, limit_upcoming: int) -> dict:
     
     data_dict = {
         'fq': 'availability:upcoming',
+        'sort': 'metadata_created desc',
         'rows': limit_upcoming
     }
     package_list_upcoming = toolkit.get_action('package_search')(context = {'ignore_auth': True}, data_dict = data_dict)
